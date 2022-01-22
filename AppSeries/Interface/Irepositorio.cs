b5a5b7190258a16b41e31edd69054f4b5c3ced9d
vitorlimao <vitor.lima.s@live.com>
2022-01-22 07:46:49 -0300

    update repository

---
+++ AppSeries/Interface/Irepositorio.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+namespace AppSeries.Interface
+{
+    public interface Irepositorio<T>
+    {
+         List<T> Lista ();
+
+         T RetornaPorId(int id);
+
+         void Insere ( T entidade);
+
+         void Exclui (int id);
+
+         void Atualiza (int id, T entidade);
+
+         int ProximoId();
+
+
+    }
+}
